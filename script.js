289296ad110d13b1a16c96cd616de02fb808db11
aadumensah02-cloud <aadumensah02@gmail.com>
2026-09-20 16:33:34 +0000

    Added the code to aid in the responsive design and allow interactivity

--- script.js
+++ script.js
@@ -8,3 +8,32 @@ window.addEventListener('scroll', () => {
     btn.classList.remove('fixed');
   }
 });
+
+const mobileButton = document.querySelector('.mobile-button');
+const navLinks = document.querySelector('#nav-links');
+const closeBtn = document.querySelector('.close-button');
+
+mobileButton.addEventListener('click', () => {
+  navLinks.classList.add('mobile-open');
+  mobileButton.style.display = 'none';
+  closeBtn.style.display = 'block';
+  document.documentElement.classList.add('menu-open');
+  document.body.classList.add('menu-open');
+});
+
+closeBtn.addEventListener('click', () => {
+  navLinks.classList.remove('mobile-open');
+  closeBtn.style.display = 'none';
+  mobileButton.style.display = 'block';
+  document.documentElement.classList.remove('menu-open');
+  document.body.classList.remove('menu-open');
+});
+
+
+document.querySelectorAll('.accordion-toggle').forEach(toggle => {
+    toggle.addEventListener('click', () => {
+        const panel = toggle.nextElementSibling;
+        toggle.classList.toggle('open');
+        panel.classList.toggle('open');
+    });
+});
